d6e84e423d65e3e75805e022c9d5bdcc719d3d5b
jack9761 <jackmietz@gmail.com>
2024-07-07 18:16:02 -0700

    Bullets Fixed

--- scenes/fish/bullets/bullet_base.gd
+++ scenes/fish/bullets/bullet_base.gd
@@ -2,15 +2,17 @@ extends StaticBody3D
 class_name Bullet
 var pos
 var target: Vector3
-
+var timestampInstaniated:float
 func _spawn(_pos: Vector3, tar: Vector3):
 	pos = _pos
 	target = tar
 func _ready():
 	global_position = pos
-
+	timestampInstaniated = Time.get_unix_time_from_system()
 func _process(_delta):
-	global_position = global_position.move_toward(target,1)
+	global_position = global_position.move_toward(target,.1)
 	var col = move_and_collide(Vector3.ZERO,true)
 	if col && col.get_collider() != auto.player:
+		queue_free()
+	if Time.get_unix_time_from_system()> timestampInstaniated+30:
 		queue_free()
