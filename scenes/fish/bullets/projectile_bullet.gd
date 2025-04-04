23c60498ad17b9b0bab9c9cd2824784e7767fe15
jack9761 <jackmietz@gmail.com>
2025-04-03 21:45:48 -0700

    Bullets work correctly at point-blank

--- scenes/fish/bullets/projectile_bullet.gd
+++ scenes/fish/bullets/projectile_bullet.gd
@@ -25,8 +25,8 @@ func _ready() -> void:
 	mesh.mesh = res.mesh
 	add_child(mesh)
 	if explode_immediatly:
-		damage(global_position)
 		moving = false
+		damage(target_position)
 		return
 	
 	
@@ -45,43 +45,44 @@ func _physics_process(delta: float) -> void:
 			global_position = get_collision_point()
 			damage(get_collision_point())
 			return
-		else:
-			auto.line(global_position,global_position + target_position,Color.WHITE_SMOKE,0,false)
+		#else:
+			#auto.line(global_position,global_position + target_position,Color.WHITE_SMOKE,0,false)
 		position += target_position
 
 func damage(pos:Vector3):
+	if explode_immediatly:
+		await get_tree().process_frame
 	var area = Area3D.new()
 	var collision = CollisionShape3D.new()
 	var shape = SphereShape3D.new()
 	shape.radius=res.aoe_radius
 	collision.shape = shape
-	add_child(area)
+	auto.add_child(area)
 	area.add_child(collision)
 	area.collision_mask = 0b00000000_00000000_00000000_00000101
 	area.global_position = pos
 	await get_tree().physics_frame
+	if explode_immediatly:
+		await get_tree().physics_frame
+	print(collision.global_position)
+	print(area.get_overlapping_bodies())
 	for collide:PhysicsBody3D in area.get_overlapping_bodies():
 		if collide is Player:
 			#print(res.knockback_falloff.sample(pos.distance_to(collide.position))," , ",pos.distance_to(collide.position))
-			
 			var player:Player = collide
-			player.knockback += pos.direction_to(player.position) * res.knockback_falloff.sample(pos.distance_to(collide.position)) * res.knockback
+			player.knockback += pos.direction_to(player.camera.global_position) * res.knockback_falloff.sample(pos.distance_to(collide.position)) * res.knockback
 			print(pos.direction_to(player.position),',',player.knockback)
-			
-	
 	#var meshinst = MeshInstance3D.new()
 	#var mesh2 = SphereMesh.new()
 	#mesh2.height = res.aoe_radius*2
 	#mesh2.radius = res.aoe_radius
 	#meshinst.mesh = mesh2
+	#auto.add_child(meshinst)
 	#meshinst.global_position = pos
 	#var mat = StandardMaterial3D.new()
 	#meshinst.set_surface_override_material(0,mat)
 	#mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 	#mat.albedo_color = Color(1.0,0.5,0.5,0.2)
-	#auto.add_child(meshinst)
-	
-	
 	effect_explode(pos)
 
 func effect_explode(pos:Vector3):
