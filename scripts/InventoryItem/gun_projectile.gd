d45ef12d08e4f1a33ba96aa00a57d16cf0d6948b
Spacebear9 <offline00110001@gmail.com>
2025-04-05 19:37:41 -0700

    fix file names

--- scripts/InventoryItem/gun_projectile.gd
+++ scripts/InventoryItem/gun_projectile.gd
@@ -1,14 +1,24 @@
 extends Gun
 
+@export var projectileresource : ProjectileRes
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	super()
 func _process(delta):
+	super(delta)
 	pass
-
-func _fire(launch: Vector3,target: Vector3):
-	pass
+func _fire(launch: Vector3, target: Vector3):
+	anim.play("shoot")
+	var space_state = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.exclude = [auto.get_players()[0]]
+	ray_query.from = auto.get_players()[0].camera.global_position
+	ray_query.to = launch
+	var result = space_state.intersect_ray(ray_query)
+	var explode_immediatly = (result.size() > 0)
+	if explode_immediatly:
+		auto.root.add_child(Bullet.new(projectileresource, result["position"], target, explode_immediatly))
+	else:
+		auto.root.add_child(Bullet.new(projectileresource, launch, target, explode_immediatly))
+func get_icon():
+	return Inventoryresource.Icon
