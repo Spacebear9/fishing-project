473f3c0a3d3f4a4996ba62cf986b614f185e8c9d
jack9761 <jackmietz@gmail.com>
2025-04-07 15:25:35 -0700

    Map name gets correctly

--- scripts/MapResource.gd
+++ scripts/MapResource.gd
@@ -10,29 +10,41 @@ class_name MapResource
 @export_tool_button("Read Properties from Map") var read_map_properties_button = read_map_properties
 @export var UseMapNameFromRootNode: bool = true
 @export_category("MapProperties")
+# Note: Renamed the internal variable slightly to avoid conflict/confusion
+var _map_name: String
 @export var MapName: String:
+	get:
+		return _map_name
 	set(new_name):
 		if !UseMapNameFromRootNode:
-			MapName = new_name
+			_internal_set_map_name(new_name)
 @export var MapScreenshot: Texture2D
 @export var SpawnPointArray: Array[NodePath] = []
+func _internal_set_map_name(new_name: String):
+	_map_name = new_name
+	notify_property_list_changed()
+
 func read_map_properties():
 	if Map == null:
 		return
-	var mapInstance = Map.instantiate()
-	if UseMapNameFromRootNode:
-		MapName = mapInstance.name
-	for child in mapInstance.get_children():
-		if child is SpawnPoint:
-			SpawnPointArray.append(child.get_path())
-	var allnodes = recurivelygetchildren(mapInstance)
-	for node in allnodes:
-		if node is SpawnPoint:
-			SpawnPointArray.append(node.get_path())
+	SpawnPointArray.clear()
+	var mapInstance: SceneState = Map.get_state()
+	var found_root_name = false 
+	for i in range(mapInstance.get_node_count()):
+		#print(mapInstance.get_node_instance(i))
+		#print(mapInstance.get_node_name(i))
+		if UseMapNameFromRootNode and !found_root_name and mapInstance.get_node_path(i) == NodePath("."):
+			_internal_set_map_name(mapInstance.get_node_name(i))
+			found_root_name = true
+		if mapInstance.get_node_instance(i):
+			for j in range(mapInstance.get_node_instance(i).get_state().get_node_count()):
+				print(mapInstance.get_node_instance(i).get_state().get_node_type(j))
+			if mapInstance.get_node_instance(i).get_state().get_node_type(0)=="SpawnPoint":
+				SpawnPointArray.append(mapInstance.get_node_path(i))
 	notify_property_list_changed()
 	
 func recurivelygetchildren(node: Node)-> Array[Node]:
-	var children = []
+	var children: Array[Node] = []
 	for child in node.get_children():
 		children.append(child)
 		if child.get_child_count() > 0:
