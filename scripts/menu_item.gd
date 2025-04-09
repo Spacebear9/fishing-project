1199f89c64320643c06e1984baf38b95f757f3b3
Spacebear9 <offline00110001@gmail.com>
2025-04-08 20:22:04 -0700

    fix jack suggestions

--- scripts/menu_item.gd
+++ scripts/menu_item.gd
@@ -1,21 +1,10 @@
 extends Control
 class_name MenuItem
 
-var menu:UiHandler
+var menu:UIHandler
 @export var escapable = true
 
 func _ready() -> void:
 	menu = get_parent()
-	if menu is not UiHandler:
+	if menu is not UIHandler:
 		auto.unique_error("parent node is not UiHandler, MenuItem should always be a child of UiHandler",self)
-	_start()
-
-func _process(delta: float) -> void:
-	if visible:
-		_script(delta)
-
-func _start() -> void:
-	pass
-
-func _script(_delta:float) -> void:
-	pass
