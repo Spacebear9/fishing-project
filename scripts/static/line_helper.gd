08f724cd068903c40776a731c6581bb4f6c0d20c
Spacebear9 <135493326+Spacebear9@users.noreply.github.com>
2026-02-09 18:53:06 -0800

    Update line_helper.gd

--- scripts/static/line_helper.gd
+++ scripts/static/line_helper.gd
@@ -1,10 +1,12 @@
 class_name LineHelper
 
 ##Returns a Meshinstance3D containing a line between 2 points in 3D space
-static func create_line(pos1: Vector3, pos2: Vector3,free_signal = null, color = Color.BLACK,on_top = true,):
+static var top_layer = 0b10000000
+
+static func create_line(pos1: Vector3, pos2: Vector3,free_signal = null, color = Color.BLACK,on_top = true) -> MeshInstance3D:
 	var mesh_instance := MeshInstance3D.new()
-	if on_top:
-		mesh_instance.layers = 0b1000000
+	if on_top == true:
+		mesh_instance.layers = top_layer
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
 	mesh_instance.top_level = true
 	
@@ -20,6 +22,6 @@ static func create_line(pos1: Vector3, pos2: Vector3,free_signal = null, color =
 	mesh_instance.mesh = immediate_mesh
 	
 	if free_signal is Signal:
-		free_signal.connect(mesh_instance.queue_free)
+		free_signal.connect(mesh_instance.queue_free.call_deferred)
 	
 	return mesh_instance
